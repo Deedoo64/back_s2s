d3330a8d59ad6b516d332e1b7f148098488d20e9
Didier <dlascaux@yahoo.fr>
2024-04-11 14:35:46 +0200

    Add route /version to know backend version

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/version", function (req, res, next) {
+  var version = "1.1";
+  console.log(`GET /version : ${version}`);
+  res.json({ result: true, version: version });
 });
 
 module.exports = router;
